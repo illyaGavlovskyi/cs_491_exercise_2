206c636c40af078cee31900c979d2bef95cb5680
Illya Gavlovskyi <gavlovskyi17@gmail.com>
2025-06-12 20:33:27 -0700

    8:30

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const box = document.getElementById("board");
 const control = document.getElementById("control");
 
 var board = Array(9).fill("");
+var currentPlayer = "X";
+var gameOver = false;
+var moveCount = 0;
 
-function renderBoard() {
-    
-}
