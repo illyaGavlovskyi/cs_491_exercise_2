cc66d7fda2e132db40f3103dc8eb6950c49e7d1b
illyaGavlovskyi <151693791+illyaGavlovskyi@users.noreply.github.com>
2025-06-13 17:20:41 -0700

    5:18

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const winConditions = [
 var boardPlacement = Array(9).fill("");
 var currentPlayer = "X";
 var moveCount = 0;
+var playerMoveCount = 0;
 var gameOver = false;
 var startPhase = true;
 
@@ -17,7 +18,7 @@ startGame();
 
 function startGame() {
     controler.addEventListener("click", clickButton);     
-    cells.forEach((cell => cell.addEventListener("click", clickCell)));
+    cells.forEach((cell, index) => cell.addEventListener("click", () => clickCell(index)));
 }
 
 function clickButton() {
@@ -26,23 +27,70 @@ function clickButton() {
     statusText.textContent = "Player X's turn make 2 moves";
 }
 
-function clickCell() {
-    if(gameOver) {
+function clickCell(index) {
+    if(gameOver === true) {
         return;
     }
-
+    if(boardPlacement[index] !== ""){
+        return;
+    }
+    updateCell(cells[index], index);
+    playerMoveCount++;
+    moveCount++;
+    if (startPhase && playerMoveCount < 2) {
+        statusText.textContent = "Player X: make one more move";
+        return;
+    }
+    var result = checkWin();
+    if (result.win === true) {
+        endGame(result);
+        return;
+    }
+    if(playerMoveCount >= 2){
+        startPhase = false;
+        setTimeout(computerMove, 1000);
+    }
 }
 
 function updateCell(cell, index){
+    boardPlacement[index] = currentPlayer;
+    cell.textContent = currentPlayer;
+}
+
+function computerMove() {
+    if(gameOver === true) {
+        return;
+    }
+
+    swopPlayer();
+    let move = getComputerMove();
+    updateCell(cells[move], move);
+    moveCount++;
+    swopPlayer();
 
 }
 
 function swopPlayer() {
-
+    if(currentPlayer === "X") {
+        currentPlayer = "O";
+    }
+    else {
+        currentPlayer = "X";
+    }
 }
 
 function checkWin() {
 
+}
+
+function swopPlayer() {
+    if(currentPlayer === "X"){
+        if(playerMoveCount >= 2){
+            currentPlayer = "O";
+        }
+    } else {
+        currentPlayer = "X";
+    }
 }
 
 function resetGame() {
